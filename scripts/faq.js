67b4a94d18603448612e96876481af36d702c0a5
sleepREVprivation <ramida.u.36@gmail.com>
2025-01-16 02:34:45 +0800

    self rotate and explosion animations

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const expandButtons = document.querySelectorAll('.accordion');
+    const expandButtons = document.querySelectorAll('.accordion');  // find all buttons (accordian class)
 
     expandButtons.forEach(button => {
         button.addEventListener('click', () => {
             const expandContent = button.nextElementSibling;
-            button.classList.toggle('active')
+            button.classList.toggle('active') // change symbol +/-
             if (expandContent.style.display === 'block') {
                 expandContent.style.display = 'none';
             } else {
--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,7 @@ import {
   TweakpaneUiPlugin,
   MaterialConfiguratorPlugin,
   GLTFAnimationPlugin,
+  timeout,
 
   // Import THREE.js internals
   Color,
@@ -26,6 +27,8 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 async function setupViewer() {
+
+  // SHOE1: HERO
   const viewer = new ViewerApp({
       canvas: document.getElementById('web-canvas-1'),
   });
@@ -39,12 +42,8 @@ async function setupViewer() {
 
   // Load an environment map if not set in the glb file
   await viewer.setEnvironmentMap("./assets/autumn forest.hdr");
-
-  // await manager.addFromPath("./assets/shoe.glb");
-  
   await viewer.load("./assets/shoe.glb");
 
-  const gltfAnimation = viewer.getPlugin(GLTFAnimationPlugin);
   const interactionPromptPlugin = viewer.getPlugin(InteractionPromptPlugin);
   interactionPromptPlugin.enabled = false;
 
@@ -52,25 +51,23 @@ async function setupViewer() {
   
   // disable user interaction, auto rotate model
   var controls = viewer.scene.activeCamera.controls; 
-  controls.autoRotate = true; 
+  controls.autoRotate = false; // 
   controls.enabled = false; 
   controls.enableZoom = false; 
   controls.enablePan = false; 
   controls.enableDamping = false; 
-  controls.enableRotate = false; 
+  controls.enableRotate = false;
   controls.autoRotateSpeed = 8;
 
   // set shoe model orientation
   viewer.scene.activeCamera.position = new Vector3(11, 0.2, 0.08);
   viewer.scene.activeCamera.target = new Vector3(-0.02, -0.05, -0.06);
 //  gltfAnimation.playAnimation();
-  
-
 
   // let scrollSection = document.getElementById("scrollSection");
   // await viewer.getPlugin(new ScrollableCameraViewPlugin(scrollSection));
 
-
+  // SHOE2: 360
   const viewerSpin = new ViewerApp({
     canvas: document.getElementById('web-canvas2'),
   });
@@ -82,36 +79,68 @@ async function setupViewer() {
   const interactionPromptPlugin2 = viewerSpin.getPlugin(InteractionPromptPlugin);
   interactionPromptPlugin2.enabled = false;
 
-  viewerSpin.scene.activeCamera.position = new Vector3(4.4, 4.35, 7.68);
+  // set user camera controls
+  var controlsSpin = viewerSpin.scene.activeCamera.controls;
+  controlsSpin.autoRotate = false;
+  controlsSpin.enabled = false; 
+  controlsSpin.enableZoom = false; 
+  controlsSpin.enablePan = false; 
+  controlsSpin.enableDamping = false; 
+  controlsSpin.enableRotate = true;  
+  
+  viewerSpin.scene.activeCamera.position = new Vector3(4.4, 4.35, 14); // webgi values: 4.4, 4.35, 7.68
   viewerSpin.scene.activeCamera.target = new Vector3(0, 0, 0); // webgi values: -1.48, 0, 1.5
 
+  // SHOE3: EXPLOSION
+  const viewerExplode = new ViewerApp({
+    canvas: document.getElementById('web-canvas3'),
+  });
+  await addBasePlugins(viewerExplode);
+  viewerExplode.renderer.refreshPipeline();
+  await viewerExplode.setEnvironmentMap("./assets/autumn forest.hdr");
+  await viewerExplode.load("./assets/shoe.glb");
+
+  const interactionPromptPlugin3 = viewerExplode.getPlugin(InteractionPromptPlugin);
+  interactionPromptPlugin3.enabled = false;
+
+  // set user camera controls
+  var controlsExplode = viewerExplode.scene.activeCamera.controls;
+  controlsExplode.autoRotate = false; 
+  controlsExplode.enabled = false; 
+  controlsExplode.enableZoom = false; 
+  controlsExplode.enablePan = false; 
+  controlsExplode.enableDamping = false; 
+  controlsExplode.enableRotate = false;
+  
+  // model orientation
+  viewerExplode.scene.activeCamera.position = new Vector3(19, -0.51, 1.9);  // webgi values: 19, -0.51, 1.9
+  viewerExplode.scene.activeCamera.target = new Vector3(5, -0.7, 0);  // webgi values: 0, 0, 0
+ 
+  const gltfAnimation = viewerExplode.getPlugin(GLTFAnimationPlugin); // get model animation
+  gltfAnimation.loopAnimations = false; 
+  gltfAnimation.animationSpeed = 2;
+ 
+  async function explodeShoe() { 
+    gltfAnimation.playAnimation(); 
+    await timeout(3000);  // miliseconds to wait for animation to end
+    gltfAnimation.pauseAnimation(); 
+  } 
+ 
+  async function unexplodeShoe() { 
+    gltfAnimation.playAnimation(); 
+  } 
+ 
+  ScrollTrigger.create({ 
+    trigger: "#shoe-explosion",
+    start: "top 40%",
+    end: "bottom top", // end when bottom of section reaches top of screen
+    onEnter: () => { 
+        explodeShoe(); // explode shoe when entering
+    }, 
+    onLeaveBack: () => { 
+        unexplodeShoe();  // put the shoe back when leaving
+    }, 
+  });
 }
 
 setupViewer();
-
-// window.onscroll = function() {
-//   if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-//       document.querySelector('.back-to-top').style.display = 'block';
-//   } else {
-//       document.querySelector('.back-to-top').style.display = 'none';
-//   }
-// };
-
-// let scrollSpeed = 1.0;
-
-// // Add an event listener for the 'wheel' event
-// document.addEventListener('wheel', function(event) {
-//   // Prevent default scrolling behavior
-//   event.preventDefault();
-
-//   // Calculate the new scroll position
-//   let delta = event.deltaY;
-//   let scrollPosition = window.scrollY + (delta * scrollSpeed);
-
-//   // Set the new scroll position
-//   window.scrollTo({
-//     top: scrollPosition,
-//     behavior: 'smooth'
-//   });
-// },
-// { passive: false });
